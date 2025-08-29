46eae900e8c31a400e04f09fffe2f38098471140
luis-kael <sithukaungsett2001@gmail.com>
2025-08-30 00:36:17 +0630

    day1

---
+++ 1_variable.py
@@ -0,0 +1,29 @@
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+# Integers
+
+age = 25 
+quantity = 3
+num_of_students = 30
+
+# Float
+
+price = 10.99
+gpa = 3.2
+distance = 5.5 
+
+#Boolean
+
+is_student = True
+for_sale = True
+is_online = True
+
+if is_student:
+    print("You are online")
+    
+else:
+    print("You are offline")
+    
---
+++ 2_typecasting.py
@@ -0,0 +1,9 @@
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
+
+gpa = int(gpa)
+
+print(gpa)
+

db5b9b6b406de8eda4bbe497e2f26833be369bdc
luis-kael <sithukaungsett2001@gmail.com>
2025-09-08 23:41:34 +0630

    day9

---
+++ 27_variable_scope.py
@@ -0,0 +1,10 @@
+def func1():
+    x = 1
+    print(x)
+    
+def func2():
+    x = 2
+    print(x)
+    
+func1()
+func2()
---
+++ mini_projects/Python_slots_machine.py
@@ -0,0 +1,49 @@
+import random
+
+def spin_row():
+    symbols = ['🍒', '🍉', '🍋', '🔔', '⭐']
+
+    return [random.choice(symbols) for _ in range(3)]
+   
+   
+    
+def print_row(row):
+    print_row(" | ".join(row))
+
+def get_payout():
+    pass
+
+def main():
+    balance = 100
+    
+    print("*************************")
+    print("Welcome to Python Slots ")
+    print("Symbols: 🍒 🍉 🍋 🔔 ⭐")
+    print("*************************")
+    
+    while balance > 0:
+        print(f"Current balance: ${balance}")
+        
+        bet = input("Place your bet amount: ")
+
+        if not bet.isdigit():
+            print("Please enter a valid number")
+            continue
+        
+        bet = int(bet)
+        if bet > balance:
+            print("Insuffient funds")
+            continue
+        
+        if bet <= 0:
+            print("Bet must be greater than 0")
+            continue
+        
+        balance -= bet
+        
+        row = spin_row()
+        print("Spinning...\n")
+        print_row(row)
+    
+if __name__ == '__main__':
+    main()

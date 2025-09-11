dd87dd6e32278a58833380de0512fd14400e3d22
luis-kael <sithukaungsett2001@gmail.com>
2025-09-11 23:05:56 +0630

    day11

--- mini_projects/Encryption_program.py
+++ mini_projects/Encryption_program.py
@@ -2,5 +2,22 @@ import random
 import string
 
 chars = string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
 
-print(chars)
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key: {key}")
+
+#ENCRYPT
+
+plain_text = input("ENter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+    
+print(f"orginal message: {plain_text}")
+print(f"encrypted message: {cipher_text}")

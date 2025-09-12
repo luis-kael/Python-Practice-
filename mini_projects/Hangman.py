85a549e2c7caed9b685a923add57ad08e5239812
luis-kael <sithukaungsett2001@gmail.com>
2025-09-13 00:31:56 +0630

    day11

--- mini_projects/Hangman.py
+++ mini_projects/Hangman.py
@@ -49,11 +49,34 @@ def main():
         display_hint(hint)
         guess = input("Enter a letter: ").lower()
         
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input")
+            continue
+        
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+        
+        guessed_letters.add(guess)
+        
         if guess in answer:
             for i in range(len(answer)):
                 if answer[i] == guess:
                     hint[i] = guess
+        else:
+            wrong_guesses += 1
         
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN!")
+            is_running = False
+            
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE!")
+            is_running = False
         
         
 if __name__ == "__main__":

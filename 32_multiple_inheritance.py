ae31a32e48edcd7b756e8c185f911b9acf8b0c88
luis-kael <sithukaungsett2001@gmail.com>
2025-09-14 21:07:18 +0630

    day12

--- 32_multiple_inheritance.py
+++ 32_multiple_inheritance.py
@@ -1,1 +1,34 @@
-# multiple inheritance
+# multiple inheritance and multi_level inheritance
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        
+        
+    def eat(self):
+        print(f"{self.name} is eating")
+    
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+        
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Bugs")
+hawk = Hawk("Tony")
+fish = Fish("Nemo")
+

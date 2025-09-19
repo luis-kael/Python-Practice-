a10c49d6dfca81afb81d1e0364f30654a0c4d339
luis-kael <sithukaungsett2001@gmail.com>
2025-09-19 12:17:11 +0630

    day15

---
+++ PyQt5/4_Layouts.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
+                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+
+class Main_Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+    
+    def initUI(self):
+        pass
+     
+        
+def main():
+    app = QApplication(sys.argv)
+    window = Main_Window()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+    
+        
